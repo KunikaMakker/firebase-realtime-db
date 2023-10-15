618ce8be55afc6a5ea6a39f95b58f12fdbb24ba0
Kunika <kunika@Kunikas-MacBook-Air.local>
2023-10-15 19:05:41 +0530

    setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'firebase-realtime-db';
+  todoItem: any = '';
+  todolist: any[] = []
+  editMode: boolean = false;
+  editableItem: string = '';
+
+  constructor() {
+    //constructor
+  }
+
+  ngOnInit(): void {
+    //on initialise
+  }
+
+  saveTodoItem() {
+    //save item
+  }
+
+  editItem(index: number) {
+    //edit
+    this.editableItem = this.todolist[index];
+    this.editMode = true;
+  }
+
+  updateItem() {
+    //update item
+    this.editMode = false;
+  }
+
+  deleteItem(index: number) {
+    //delete item
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCwv3VVHngBCcnUcjFDlPbc4tc9RhfrP9I",
+    authDomain: "fir-realtime-db-4e639.firebaseapp.com",
+    databaseURL: "https://fir-realtime-db-4e639-default-rtdb.firebaseio.com",
+    projectId: "fir-realtime-db-4e639",
+    storageBucket: "fir-realtime-db-4e639.appspot.com",
+    messagingSenderId: "989789086995",
+    appId: "1:989789086995:web:b771b1c2ceb639f8a0e6e6",
+    measurementId: "G-Q3H84DKX51"
+  }
 };
 
 /*
